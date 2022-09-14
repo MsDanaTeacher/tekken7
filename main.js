b803dfea325c176da08c9de444ea4d4eb674a405
Dana Lobell <danalobell@gmail.com>
2022-09-14 16:09:24 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+function one() {
+    let names = ['akuma', 'alisa' ,'anna', 'asuka', 'bob', 'bryan', 'claudio', 'dragunov', 'eddy', 'eliza', 'feng', 'geese']
+    const fighterUrl = (name) => {
+      return `https://tk7.tekken.com/assets/images/fighters-final/${name}-standard/thumbnail-headshot.png`
+    }
+    return (
+      <>
+      <div className="backg">
+        <div className="fighters">
+          {names.map((el, i) => {
+            return <img className="each" src={fighterUrl(el)} />
+          })}
+        </div>
+        </div>
+      </>
+    );
+  }
